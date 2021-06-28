d1d8c379914c64e9f618cbe87261feb5c52ca34f
Maya Tagunova <maya.tagunova@gmail.com>
2021-06-28 20:50:13 +0300

    Initial commit

---
+++ polls/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+        ('Type', {'fields': ['type']})
+    ]
+
+    list_display = ['question_text', 'pub_date', 'was_published_recently']
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    inlines = [ChoiceInline]
+
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ['pub_date']
+        else:
+            return []
+
+admin.site.register(Question, QuestionAdmin)
